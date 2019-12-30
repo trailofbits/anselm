09f134a7d2956751d6ddde8febe924754073ef93
William Wang <defund@users.noreply.github.com>
2019-12-30 17:53:37 -0500

    Graph.cpp documentation

--- Graph.cpp
+++ Graph.cpp
@@ -12,16 +12,27 @@ namespace {
 		static char ID;
 		GraphPass() : ModulePass(ID) { }
 
+		// isRelevant checks whether an individual instruction should be
+		// included in the final graph.
 		bool isRelevant(Instruction &I, unordered_set<string> allowed) {
 			auto B = I.getParent();
+
+			// first instruction of a block that has no predecessors
 			if (pred_begin(B) == pred_end(B) && I.isIdenticalTo(&B->front()))
 				return true;
+
+			// last instruction of a block that has no successors
 			if (succ_begin(B) == succ_end(B) && I.isIdenticalTo(&B->back()))
 				return true;
+
+			// instruction is a call to a function
 			if (auto call = dyn_cast<CallInst>(&I)) {
 				if (auto func = call->getCalledFunction()) {
+					// marked for analysis
 					if (allowed.find(func->getName()) != allowed.end())
 						return true;
+
+					// defined in bitcode
 					if (!func->isDeclaration())
 						return true;
 				}
@@ -29,11 +40,20 @@ namespace {
 			return false;
 		}
 
+		// getGraph creates a directed graph for a function with nodes
+		// representing relevant instructions. Edges represent possible
+		// paths of execution.
 		dg getGraph(Function &F, unordered_set<string> allowed) {
 			dg graph;
+
+			// populate graph
 			for (auto& B : F) {
+				// always include the first instruction of a block for
+				// intermediate use
 				auto node = &B.front();
 				graph[node];
+
+				// create a linked list of relevant instructions within a block
 				for (auto& I : B) {
 					if (isRelevant(I, allowed) && !I.isIdenticalTo(node)) {
 						graph[node].second.emplace(&I);
@@ -41,40 +61,55 @@ namespace {
 						node = &I;
 					}
 				}
+
+				// create edges between blocks' linked lists
 				for (auto it = succ_begin(&B); it != succ_end(&B); ++it) {
 					auto edge = &(*it)->front();
 					graph[edge].first.emplace(node);
 					graph[node].second.emplace(edge);
 				}
 			}
+
+			// compress graph by removing irrelevant first instructions
+			// while maintaining overall paths
 			for (auto& B : F) {
 				if (!isRelevant(B.front(), allowed)) {
 					auto node = &B.front();
 					auto& edges = graph.at(node);
+
+					// remove node from all predecessors
 					for (auto& edge : edges.first) {
 						auto& succs = graph.at(edge).second;
 						succs.erase(node);
 						for (auto succ : edges.second)
 							succs.emplace(succ);
 					}
+
+					// remove node from all successors
 					for (auto& edge : edges.second) {
 						auto& preds = graph.at(edge).first;
 						preds.erase(node);
 						for (auto pred : edges.first)
 							preds.emplace(pred);
 					}
+
 					graph.erase(node);
 				}
 			}
+
 			return graph;
 		}
 
+		// runOnModule collects all function graphs and runs analyses
 		virtual bool runOnModule(Module &M) override {
-			unordered_map<Function*, dg> graphs;
 			auto allowed = getAllowed();
+			unordered_map<Function*, dg> graphs;
+
+			// only graph functions defined in bitcode
 			for (auto& F : M)
 				if (!F.isDeclaration())
 					graphs[&F] = getGraph(F, allowed);
+
 			runAnalysis(graphs);
 			return false;
 		}
